4d31ae8f9572da4303262665a3cd3b8901608473
Filip P. <pawik043@gmail.com>
2026-05-03 19:16:48 +0300

    Implement addCommand() environment

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,21 @@
-import { Plugin } from 'obsidian'
+import { Editor, Plugin, Notice } from 'obsidian'
 
 export default class YomiNaru extends Plugin {
-	onload(): void {
-		console.log("Hello World!")
+	async onload() {
+		console.log("Loading YomiNaru")
+		this.addCommand({
+			id: "yn-process-note",
+			name: "Kanjify",
+			hotkeys: [{ modifiers: ['Mod'], key:'y'}],
+			editorCallback: (editor: Editor) => {
+
+			}
+		})
 	}
+
+	async onunload() {
+		console.log("Unloading YomiNaru :(")
+	}
+
+
 }
